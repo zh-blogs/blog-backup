5503295eb9209b642e38813c6fe9187a7824d217
xcsoft <email@xcsoft.top>
2022-05-02 16:52:21 +0800

    fix

--- main.go
+++ main.go
@@ -6,11 +6,16 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 var apiUrl = "https://zhblogs.ohyee.cc/api/blogs?size=-1&status=0"
+var maxNum = 3
+var dataDir = "./database"
 
 func main() {
+	log.Print("getting blogs...")
 	if res, err := http.Get(apiUrl); err == nil {
 		defer func(Body io.ReadCloser) {
 			_ = Body.Close()
@@ -46,9 +51,55 @@ func parseData(blogs []interface{}) {
 	if data, err := json.Marshal(blogs); err != nil {
 		log.Fatalf("error when marshal blogs: %s", err)
 	} else {
+		//check if database dir exists
+		makeDataDir()
+		// if more than %maxNum% files, delete the oldest one
+		deleteOldestFile()
 		// write to ./tmp/data.json
-		if err = ioutil.WriteFile("./data.json", data, 0644); err != nil {
-			log.Fatalf("error when write data.json: %s", err)
+		writeToFile(data)
+	}
+}
+
+func writeToFile(data []byte) {
+	timeNow := time.Now().Format("01-02-15:04:05")
+	path := dataDir + "/data-" + timeNow + ".json"
+
+	if err := ioutil.WriteFile(path, data, 0644); err == nil {
+		log.Printf("write to %s", path)
+	} else {
+		log.Fatalf("error when write to %s: %s", path, err)
+	}
+}
+
+func deleteOldestFile() {
+	if list, err := ioutil.ReadDir("database"); err == nil {
+		for _, file := range list {
+			if getDataFileNum() >= maxNum {
+				if err := os.Remove(dataDir + "/" + file.Name()); err == nil {
+					log.Printf("delete %s", file.Name())
+				} else {
+					log.Printf("error when delete file: %s", err)
+				}
+			}
+		}
+	} else {
+		log.Fatalf("error when read database dir: %s", err)
+	}
+}
+
+func getDataFileNum() int {
+	if list, err := ioutil.ReadDir(dataDir); err == nil {
+		return len(list)
+	} else {
+		log.Printf("error when read database dir: %s", err)
+	}
+	return 0
+}
+
+func makeDataDir() {
+	if _, err := ioutil.ReadDir(dataDir); err != nil {
+		if err := os.Mkdir(dataDir, os.ModePerm); err != nil {
+			log.Fatalf("error when create database dir: %s", err)
 		}
 	}
 }
